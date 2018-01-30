8220bddaaf9e80d65386d5e0821cf71be6db38e8
Sn8z <marcus.sneitz@telia.com>
2018-01-30 17:20:33 +0100

    added window state

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const windowStateKeeper = require('electron-window-state');
 
 //Global reference to window object;
 var mainWindow = null;
@@ -12,20 +13,39 @@ app.on('window-all-closed', function(){
 
 //When app is rdy, create window
 app.on('ready', function(){
+
+  //default window size
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1200,
+    defaultHeight: 900
+  });
+
   mainWindow = new BrowserWindow({
     name: "Poddr",
-    width: 1200,
-    height: 900,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
     frame: true,
+    show: false,
     backgroundColor: "#333333",
     icon: __dirname + '/app/images/icon.png'
+  });
+
+  //add listeners to the window
+  mainWindowState.manage(mainWindow);
+
+  //when main window is ready
+  mainWindow.on('ready-to-show', function() { 
+    mainWindow.show(); 
+    mainWindow.focus(); 
   });
 
   //Point to html file to be opened
   mainWindow.loadURL('file://' + __dirname + "/app/index.html");
 
   //Devtools
-  mainWindow.webContents.openDevTools({detach:true});
+  //mainWindow.webContents.openDevTools({detach:true});
 
   //Cleanup on window close
   mainWindow.on('closed', function(){
