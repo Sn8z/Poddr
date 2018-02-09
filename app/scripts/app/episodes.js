86fc44fa5a200af38b6b758fa3a4cf0fde915259
Sn8z <marcus.sneitz@telia.com>
2018-02-09 22:04:03 +0100

    added when to show or hide the loadmore btn

--- app/scripts/app/episodes.js
+++ app/scripts/app/episodes.js
@@ -6,15 +6,20 @@ angular.module('poddr').controller(
     $scope.nrOfItems = 20;
     $scope.playPodcast = $rootScope.playPodcast;
     $scope.episodes = [];
+    $scope.canLoadMore = false;
 
     $scope.loadMore = function(){
       $scope.nrOfItems += 10;
+      if ($scope.episodes.length <= $scope.nrOfItems) {
+        $scope.canLoadMore = false;
+      }
     }
 
     $rootScope.fetchEpisodes = function (id){
       $scope.episodes = [];
       $scope.isLoading = true;
       $scope.nrOfItems = 20;
+      $scope.canLoadMore = false;
       $http.get("https://itunes.apple.com/lookup?id=" + id).then(function(response){
         $http.get(response.data.results[0].feedUrl).then(function(response){
           parsePodcast(response.data, function(err, data){
@@ -23,6 +28,9 @@ angular.module('poddr').controller(
             } else {
               $scope.isLoading = false;
               $scope.episodes = data.episodes;
+              if ($scope.episodes.length > $scope.nrOfItems) {
+                $scope.canLoadMore = true;
+              }
             }
           });
         });
