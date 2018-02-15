26635fb9f4461c8672cd897f4e9fb7771f46ad4d
Sn8z <marcus.sneitz@gmail.com>
2018-02-15 22:20:09 +0100

    added color selection

--- app/scripts/app/poddrController.js
+++ app/scripts/app/poddrController.js
@@ -1,41 +1,51 @@
 var app = angular.module('poddr')
   .controller("PoddrController", function($scope, $rootScope, $mdToast, $mdSidenav, $http){
-    //preloading modules to cache to speed up first time view of for example search page
-    var storage = require('electron-json-storage');
-    var itunesSearch = require('itunes-api-search');
-    var parsePodcast = require('node-podcast-parser');
+  //preloading modules to cache to speed up first time view of for example search page
+  var storage = require('electron-json-storage');
+  var itunesSearch = require('itunes-api-search');
+  var parsePodcast = require('node-podcast-parser');
+  let Mousetrap = require('mousetrap');
+  
+  Mousetrap.bind('space', function(e){
+    e.preventDefault();
+    $rootScope.togglePlay();
+  });
+  Mousetrap.bind('mod+s', function(e){
+    e.preventDefault();
+    $rootScope.volumeUp();
+  });
+  Mousetrap.bind('mod+a', function(e){
+    e.preventDefault();
+    $rootScope.volumeDown();
+  });
 
-    let Mousetrap = require('mousetrap');
-    Mousetrap.bind('space', function(e){
-      e.preventDefault();
-      $rootScope.togglePlay();
-    });
-    Mousetrap.bind('mod+s', function(e){
-      e.preventDefault();
-      $rootScope.volumeUp();
-    });
-    Mousetrap.bind('mod+a', function(e){
-      e.preventDefault();
-      $rootScope.volumeDown();
-    });
-
-    //check if update is available
-    $http.get("https://raw.githubusercontent.com/Sn8z/Poddr/master/package.json").then(function(response){
-      if(response.data.version != require('electron').remote.app.getVersion()){
-        var toast = $mdToast.simple()
-          .textContent(response.data.version + " available!")
-          .position("top right")
-          .hideDelay(10000)
-          .action("Update now!")
-          .toastClass('md-toast-success')
-
-        $mdToast.show(toast).then(function(response){
-          if(response == 'ok'){
-            require('electron').shell.openExternal('https://github.com/Sn8z/Poddr/releases');
-          }
-        });
+  $scope.init = function(){
+    storage.get('theme', function (error, data) {
+      var color = "#ff9900";
+      if (!error && data.value) {
+        color = data.value;
       }
+      var html = document.getElementsByTagName('html')[0];
+      html.style.cssText = "--main-color: " + color;
     });
+  }
+
+  //check if update is available
+  $http.get("https://raw.githubusercontent.com/Sn8z/Poddr/master/package.json").then(function(response){
+    if(response.data.version != require('electron').remote.app.getVersion()){
+      var toast = $mdToast.simple()
+        .textContent(response.data.version + " available!")
+        .position("top right")
+        .hideDelay(10000)
+        .action("Update now!")
+        .toastClass('md-toast-success')
+      $mdToast.show(toast).then(function(response){
+        if(response == 'ok'){
+          require('electron').shell.openExternal('https://github.com/Sn8z/Poddr/releases');
+        }
+      });
+    }
+  });
 
   $rootScope.toggleSidebar = function(){
     $mdSidenav("right").toggle();
--- app/scripts/app/settings.js
+++ app/scripts/app/settings.js
@@ -5,21 +5,39 @@ angular.module('poddr').controller(
     $scope.countries = RegionService.regions;
     $scope.region = $scope.countries[0];
 
-    $scope.openURL = function(url){
-      require('electron').shell.openExternal(url);
-    };
-
-    storage.get('region', function(error, data) {
+    storage.get('region', function (error, data) {
       if (error) throw error;
-      if(data.value.length > 0){
+      if (data.value.length > 0) {
         $scope.region = data.value;
       }
       $scope.$apply();
     });
 
+    storage.get('theme', function (error, data) {
+      if (!error && data.value){
+        $scope.color = data.value;
+      } else {
+        $scope.color = "#ff9900";        
+      }
+      $scope.$apply();
+    });
+
+    $scope.openURL = function(url){
+      require('electron').shell.openExternal(url);
+    };
+
+
+    $scope.changeColor = function(){
+      var html = document.getElementsByTagName('html')[0];
+      html.style.cssText = "--main-color: " + $scope.color;
+      storage.set("theme", { value: $scope.color }, function (err) {
+        if (err) console.log(err);
+      });
+    }
+
     $scope.setRegion = function(){
       storage.set("region", {value: $scope.region}, function(err){
         if(err) console.log(err);
-      })
+      });
     };
 });
