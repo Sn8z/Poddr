12eeea98f689c39f2cf83d15f433082035207e9d
Sn8z <marcus.sneitz@gmail.com>
2018-02-22 15:52:16 +0100

    cleaned code

--- app/scripts/app/menu.js
+++ app/scripts/app/menu.js
@@ -1,31 +1,28 @@
-(function () {	
-    	
-          const remote = require('electron').remote;	
-    	
-          function init() {	
-            document.getElementById("min-btn").addEventListener("click", function (e) {	
-              const window = remote.getCurrentWindow();	
-              window.minimize();	
-            });	
-    	
-            document.getElementById("max-btn").addEventListener("click", function (e) {	
-              const window = remote.getCurrentWindow();	
-              if (!window.isMaximized()) {	
-                window.maximize();	
-              } else {	
-                window.unmaximize();	
-              }	
-            });	
-    	
-            document.getElementById("close-btn").addEventListener("click", function (e) {	
-              const window = remote.getCurrentWindow();	
-              window.close();	
-            });	
-          };	
-    	
-          document.onreadystatechange = function () {	
-            if (document.readyState == "complete") {	
-              init();	
-            }	
-          };	
-    })();
+(function() {
+  const remote = require("electron").remote;
+
+  function init() {
+    const window = remote.getCurrentWindow();
+    document.getElementById("min-btn").addEventListener("click", function(e) {
+      window.minimize();
+    });
+
+    document.getElementById("max-btn").addEventListener("click", function(e) {
+      if (!window.isMaximized()) {
+        window.maximize();
+      } else {
+        window.unmaximize();
+      }
+    });
+
+    document.getElementById("close-btn").addEventListener("click", function(e) {
+      window.close();
+    });
+  }
+
+  document.onreadystatechange = function() {
+    if (document.readyState == "complete") {
+      init();
+    }
+  };
+})();
