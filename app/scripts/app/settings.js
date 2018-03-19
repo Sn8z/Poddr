8d1fbfc0f2767854c350286f6d52c03796e8b756
Sn8z <marcus.sneitz@gmail.com>
2018-03-19 16:10:21 +0100

    added version number to about page

--- app/scripts/app/settings.js
+++ app/scripts/app/settings.js
@@ -1,16 +1,17 @@
 angular
   .module("poddr")
-  .controller("SettingsController", function(
+  .controller("SettingsController", function (
     $scope,
     RegionService,
     $mdTheming
   ) {
     var storage = require("electron-json-storage");
+    $scope.appVersion = require('electron').remote.app.getVersion();
 
-    RegionService.regions(function(response) {
+    RegionService.regions(function (response) {
       $scope.countries = response;
       $scope.region = $scope.countries[0];
-      storage.get("region", function(error, data) {
+      storage.get("region", function (error, data) {
         if (error) {
           $scope.region = $scope.countries[0];
         } else {
@@ -24,7 +25,7 @@ angular
       });
     });
 
-    storage.get("theme", function(error, data) {
+    storage.get("theme", function (error, data) {
       if (!error && data.value) {
         $scope.color = data.value;
       } else {
@@ -33,20 +34,20 @@ angular
       $scope.$apply();
     });
 
-    $scope.openURL = function(url) {
+    $scope.openURL = function (url) {
       require("electron").shell.openExternal(url);
     };
 
-    $scope.changeColor = function() {
+    $scope.changeColor = function () {
       var html = document.getElementsByTagName("html")[0];
       html.style.cssText = "--main-color: " + $scope.color;
-      storage.set("theme", { value: $scope.color }, function(err) {
+      storage.set("theme", { value: $scope.color }, function (err) {
         if (err) console.log(err);
       });
     };
 
-    $scope.setRegion = function() {
-      storage.set("region", { value: $scope.region }, function(err) {
+    $scope.setRegion = function () {
+      storage.set("region", { value: $scope.region }, function (err) {
         if (err) console.log(err);
       });
     };
