dc8ea17dfaad22c3294cdada1df66e9690deb3b7
Sn8z <marcus.sneitz@gmail.com>
2018-06-27 23:12:43 +0200

    increased timeouts in test

--- test/test_navigation.js
+++ test/test_navigation.js
@@ -18,26 +18,26 @@ describe("Application navigation", function () {
   });
 
   it("Should be able to click toplist button", function () {
-    return this.app.client.waitForVisible('#toplist-item', 5000).element("#toplist-item").click();
+    return this.app.client.waitForVisible('#toplist-item', 30000).element("#toplist-item").click();
   });
 
   it("Should be able to click search button", function () {
-    return this.app.client.waitForVisible('#search-item', 5000).element("#search-item").click();
+    return this.app.client.waitForVisible('#search-item', 30000).element("#search-item").click();
   });
 
   it("Should be able to click favourite button", function () {
-    return this.app.client.waitForVisible('#favourite-item', 5000).element("#favourite-item").click();
+    return this.app.client.waitForVisible('#favourite-item', 30000).element("#favourite-item").click();
   });
 
   it("Should be able to click about button", function () {
-    return this.app.client.waitForVisible('#about-item', 5000).element("#about-item").click();
+    return this.app.client.waitForVisible('#about-item', 30000).element("#about-item").click();
   });
 
   it("Should be able to click toggle button", function () {
-    return this.app.client.waitForVisible('#toggle-btn', 5000).element("#toggle-btn").click();
+    return this.app.client.waitForVisible('#toggle-btn', 30000).element("#toggle-btn").click();
   });
 
   it("Should be able to click play button", function () {
-    return this.app.client.waitForVisible('#play-btn', 5000).element("#play-btn").click();
+    return this.app.client.waitForVisible('#play-btn', 30000).element("#play-btn").click();
   });
 });
