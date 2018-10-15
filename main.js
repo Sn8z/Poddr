71cc02afa346e50593164657bc70acf9e3765ed7
Marcus Sneitz <marcus.sneitz@gmail.com>
2018-10-15 14:39:54 +0200

    fixed macOS menu, fixes copy/paste issue

--- main.js
+++ main.js
@@ -67,6 +67,27 @@ app.once("ready", function () {
     });
   }
 
+  //Add menu edit menu when on Mac
+  if (process.platform == 'darwin') {
+    var menuTemplate = [{
+      label: "Poddr",
+      submenu: [
+        { label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
+      ]
+    }, {
+      label: "Edit",
+      submenu: [
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]
+    }
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+  }
+
   //default window size
   let mainWindowState = windowStateKeeper({
     defaultWidth: 1200,
@@ -102,31 +123,4 @@ app.once("ready", function () {
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
-
-  //Add menu edit menu when on Mac
-  if (process.platform == 'darwin') {
-    var menuTemplate = [{
-      label: "Application",
-      submenu: [
-        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-        { type: "separator" },
-        { label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
-      ]
-    }, {
-      label: "Edit",
-      submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-      ]
-    }
-    ];
-
-    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
-  }
-
 });
