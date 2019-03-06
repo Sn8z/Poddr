f1e7d8e1afaee9fbd9a0d19a28c728d2b6210f4d
Sn8z <marcus.sneitz@gmail.com>
2019-03-06 14:16:58 +0100

    added macOS menu

--- app/scripts/contextMenu.js
+++ app/scripts/contextMenu.js
@@ -14,5 +14,36 @@ const contextMenu = require('electron-context-menu');
             inspect: 'Poddr DevTools'
         }
     });
+
+    if (process.platform == "darwin") {
+        var menuTemplate = [
+            {
+                label: "Poddr",
+                submenu: [
+                    {
+                        label: "Quit",
+                        accelerator: "Command+Q",
+                        click: function () {
+                            app.quit();
+                        }
+                    }
+                ]
+            },
+            {
+                label: "Edit",
+                submenu: [
+                    { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+                    { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+                    { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                    {
+                        label: "Select All",
+                        accelerator: "CmdOrCtrl+A",
+                        selector: "selectAll:"
+                    }
+                ]
+            }
+        ];
+        Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+    }
 }());
 
