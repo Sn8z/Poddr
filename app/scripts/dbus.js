2295b6d6c0b57f9f1d1c0bcd3a2ef6508ff2c172
Sn8z <marcus.sneitz@gmail.com>
2019-03-07 16:11:41 +0100

    dbus fixes

--- app/scripts/dbus.js
+++ app/scripts/dbus.js
@@ -1,12 +1,28 @@
+const BrowserWindow = require("electron").BrowserWindow;
 const log = require("electron-log");
 
 (function () {
+    var mainWindow = BrowserWindow.getAllWindows()[0];
+
     async function registerDbus(desktop, bus) {
         var object = await bus.getProxyObject("org." + desktop + ".SettingsDaemon", "/org/" + desktop + "/SettingsDaemon/MediaKeys");
         var mediaKeys = object.getInterface("org." + desktop + ".SettingsDaemon.MediaKeys");
-        mediaKeys.on("MediaPlayerKeyPressed", function (iface, changed, invalidated) {
-            for (let prop of Object.keys(changed)) {
-                log.info(`property changed: ${prop}`);
+        mediaKeys.on("MediaPlayerKeyPressed", function (interface, key, error) {
+            if (error) log.error(error);
+            switch (key) {
+                case "Next":
+                    log.info("next");
+                    return;
+                case "Previous":
+                    log.info("prev");
+                    return;
+                case "Play":
+                    mainWindow.webContents.send('toggle-play');
+                    return;
+                case "Stop":
+                    log.info("stop");
+                    return;
+                default: return;
             }
         });
         mediaKeys.GrabMediaPlayerKeys("org." + desktop + ".SettingsDaemon.MediaKeys", 0);
--- app/scripts/mediakeys.js
+++ app/scripts/mediakeys.js
@@ -5,7 +5,7 @@ const log = require("electron-log");
 
 (function () {
     log.info("Loading globalshortcuts for " + process.platform);
-    mainWindow = BrowserWindow.getAllWindows()[0];
+    var mainWindow = BrowserWindow.getAllWindows()[0];
 
     //Global shortcut for Play/Pause toggle, player.js listens for the toggle-play event
     globalShortcut.register('MediaPlayPause', function () {
