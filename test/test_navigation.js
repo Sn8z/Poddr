05190c6dc02a225aea67dd19f60ada950eb2447c
Sn8z <marcus.sneitz@gmail.com>
2019-03-14 13:11:59 +0100

    updated tests for future refactoring

--- test/test_navigation.js
+++ test/test_navigation.js
@@ -6,7 +6,7 @@ describe("Application navigation", function () {
   beforeEach(function () {
     this.app = new Application({
       path: `${__dirname}/../node_modules/.bin/electron`,
-      args: ["main.js"]
+      args: ["app/main.js"]
     });
     return this.app.start();
   });
