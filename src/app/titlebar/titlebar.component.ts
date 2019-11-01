82f54e877ae98b54135c407df0a5b66c072afed2
Sn8z <marcus.sneitz@gmail.com>
2019-11-01 15:01:24 +0100

    added forward/backward btns

--- src/app/titlebar/titlebar.component.ts
+++ src/app/titlebar/titlebar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ipcRenderer as ipc } from 'electron';
 import { Location } from '@angular/common';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faWindowMinimize, faSquare } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
@@ -13,10 +13,20 @@ export class TitlebarComponent implements OnInit {
 	public faWindowMinimize = faWindowMinimize;
 	public faSquare = faSquare;
 	public faTimes = faTimes;
+	public faChevronLeft = faChevronLeft;
+	public faChevronRight = faChevronRight;
 
 	constructor(private location: Location) { }
 
 	ngOnInit() { }
+
+	public goBack() {
+		this.location.back();
+	}
+
+	public goForward() {
+		this.location.forward();
+	}
 
 	public minimize() {
 		ipc.send("window-update", "minimize");
