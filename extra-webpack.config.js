b16cbd3bc4b4cb2e988424610c6cf2574c66db42
Sn8z <marcus.sneitz@gmail.com>
2023-07-28 12:31:02 +0200

    Now supports multiple instances

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,10 +2,12 @@ const WorkerPlugin = require('worker-plugin');
 const NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin');
 
 module.exports = (config, options) => {
-  let workerPlugin = config.plugins.find(p => p instanceof WorkerPlugin);
-  if (workerPlugin) {
-    workerPlugin.options.plugins.push(new NodeTargetPlugin());
-  }
-	config.target = 'electron-renderer';
+  config.plugins.forEach((plugin) => {
+    if (plugin instanceof WorkerPlugin) {
+      plugin.options.plugins.push(new NodeTargetPlugin());
+    }
+  });
+
+  config.target = 'electron-renderer';
   return config;
 };
