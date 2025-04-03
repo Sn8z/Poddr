8dddf22b620df1da42f159d0dd8da43b71eb9e37
Marcus Sneitz <marcus.sneitz@gmail.com>
2025-04-03 17:06:15 +0200

    Update volume.dart

--- lib/ui/components/audio/volume.dart
+++ lib/ui/components/audio/volume.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poddr/ui/components/widgets/dialog.dart';
 import 'package:poddr/ui/utils/breakpoints.dart';
 import 'package:provider/provider.dart';
 import 'package:poddr/services/media.dart';
@@ -22,39 +23,32 @@ class VolumeSlider extends StatelessWidget {
       );
     }
 
-    return PopupMenuButton(
-      menuPadding: const EdgeInsets.all(0),
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      icon: const Icon(
-        Icons.volume_up_rounded,
-        size: 24,
-      ),
-      itemBuilder: (context) {
-        return [
-          PopupMenuItem(
-            enabled: false,
-            child: Selector<MediaProvider, double>(
-              selector: (_, mediaProvider) => mediaProvider.volume,
-              builder: (context, volume, child) {
-                return RotatedBox(
-                  quarterTurns: -1,
-                  child: Slider(
-                    value: volume,
-                    min: 0.0,
-                    max: 100.0,
-                    onChanged: (double value) {
-                      context.read<MediaProvider>().setVolume(value);
-                    },
-                  ),
-                );
-              },
-            ),
-          ),
-        ];
+    return IconButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return PoddrDialog(
+              children: [
+                Selector<MediaProvider, double>(
+                  selector: (_, mediaProvider) => mediaProvider.volume,
+                  builder: (context, volume, child) {
+                    return Slider(
+                      value: volume,
+                      min: 0.0,
+                      max: 100.0,
+                      onChanged: (double value) {
+                        context.read<MediaProvider>().setVolume(value);
+                      },
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        );
       },
+      icon: const Icon(Icons.volume_up),
     );
   }
 }
