e39c38cec0190e2369cd060fea240a65918751a8
Marcus Sneitz <marcus.sneitz@gmail.com>
2026-05-17 13:57:37 +0200

    updated buttons

--- lib/ui/components/widgets/poddr_buttons.dart
+++ lib/ui/components/widgets/poddr_buttons.dart
@@ -1,43 +1,103 @@
 import 'package:flutter/widgets.dart';
 import 'package:poddr/core/theme/poddr_theme.dart';
 
-class PoddrFilledButton extends StatelessWidget {
+class PoddrFilledButton extends StatefulWidget {
   final Widget child;
   final VoidCallback? onPressed;
+  final VoidCallback? onLongPress;
   final Color? backgroundColor;
   final Color? foregroundColor;
+  final BorderRadius? borderRadius;
+  final EdgeInsetsGeometry padding;
+  final String? semanticLabel;
 
   const PoddrFilledButton({
     super.key,
     required this.child,
     required this.onPressed,
+    this.onLongPress,
     this.backgroundColor,
     this.foregroundColor,
+    this.borderRadius,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+    this.semanticLabel,
   });
+
+  @override
+  State<PoddrFilledButton> createState() => _PoddrFilledButtonState();
+}
+
+class _PoddrFilledButtonState extends State<PoddrFilledButton> {
+  bool _hovering = false;
+  bool _pressing = false;
+  final _focusNode = FocusNode();
+
+  bool get _enabled => widget.onPressed != null;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = context.theme;
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        decoration: BoxDecoration(
-          color: backgroundColor ?? theme.primary,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: DefaultTextStyle(
-          style: TextStyle(
-            color: foregroundColor ?? theme.onPrimary,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-          ),
-          child: IconTheme(
-            data: IconThemeData(
-              color: foregroundColor ?? theme.onPrimary,
-              size: 20,
+    final baseColor = widget.backgroundColor ?? theme.primary;
+    final fgColor = widget.foregroundColor ?? theme.onPrimary;
+
+    Color bgColor = baseColor;
+    if (_pressing) {
+      bgColor = baseColor.withAlpha(204);
+    } else if (_hovering) {
+      bgColor = baseColor.withAlpha(230);
+    }
+
+    if (!_enabled) {
+      bgColor = baseColor.withAlpha(102);
+    }
+
+    return Semantics(
+      label: widget.semanticLabel,
+      button: true,
+      enabled: _enabled,
+      child: Focus(
+        focusNode: _focusNode,
+        child: MouseRegion(
+          cursor: _enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
+          onEnter: (_) => setState(() => _hovering = true),
+          onExit: (_) => setState(() => _hovering = false),
+          child: GestureDetector(
+            onTap: widget.onPressed,
+            onLongPress: widget.onLongPress,
+            onTapDown: _enabled ? (_) => setState(() => _pressing = true) : null,
+            onTapUp: _enabled ? (_) => setState(() => _pressing = false) : null,
+            onTapCancel: _enabled ? () => setState(() => _pressing = false) : null,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              padding: widget.padding,
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: widget.borderRadius ?? BorderRadius.circular(8),
+                border: _focusNode.hasFocus
+                    ? Border.all(color: theme.outline, width: 2)
+                    : null,
+              ),
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  color: fgColor.withAlpha(_enabled ? 255 : 128),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+                child: IconTheme(
+                  data: IconThemeData(
+                    color: fgColor.withAlpha(_enabled ? 255 : 128),
+                    size: 20,
+                  ),
+                  child: widget.child,
+                ),
+              ),
             ),
-            child: child,
           ),
         ),
       ),
@@ -45,50 +105,112 @@ class PoddrFilledButton extends StatelessWidget {
   }
 }
 
-class PoddrElevatedButton extends StatelessWidget {
+class PoddrElevatedButton extends StatefulWidget {
   final Widget child;
   final VoidCallback? onPressed;
+  final VoidCallback? onLongPress;
   final Color? backgroundColor;
   final Color? foregroundColor;
+  final BorderRadius? borderRadius;
+  final EdgeInsetsGeometry padding;
+  final String? semanticLabel;
 
   const PoddrElevatedButton({
     super.key,
     required this.child,
     required this.onPressed,
+    this.onLongPress,
     this.backgroundColor,
     this.foregroundColor,
+    this.borderRadius,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+    this.semanticLabel,
   });
+
+  @override
+  State<PoddrElevatedButton> createState() => _PoddrElevatedButtonState();
+}
+
+class _PoddrElevatedButtonState extends State<PoddrElevatedButton> {
+  bool _hovering = false;
+  bool _pressing = false;
+  final _focusNode = FocusNode();
+
+  bool get _enabled => widget.onPressed != null;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = context.theme;
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        decoration: BoxDecoration(
-          color: backgroundColor ?? theme.primary,
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: [
-            BoxShadow(
-              color: const Color(0x33000000),
-              blurRadius: 4,
-              offset: const Offset(0, 2),
+    final baseColor = widget.backgroundColor ?? theme.primary;
+    final fgColor = widget.foregroundColor ?? theme.onPrimary;
+
+    Color bgColor = baseColor;
+    if (_pressing) {
+      bgColor = baseColor.withAlpha(204);
+    } else if (_hovering) {
+      bgColor = baseColor.withAlpha(230);
+    }
+
+    if (!_enabled) {
+      bgColor = baseColor.withAlpha(102);
+    }
+
+    final shadowOpacity = _pressing ? 0.15 : (_hovering ? 0.25 : 0.2);
+
+    return Semantics(
+      label: widget.semanticLabel,
+      button: true,
+      enabled: _enabled,
+      child: Focus(
+        focusNode: _focusNode,
+        child: MouseRegion(
+          cursor: _enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
+          onEnter: (_) => setState(() => _hovering = true),
+          onExit: (_) => setState(() => _hovering = false),
+          child: GestureDetector(
+            onTap: widget.onPressed,
+            onLongPress: widget.onLongPress,
+            onTapDown: _enabled ? (_) => setState(() => _pressing = true) : null,
+            onTapUp: _enabled ? (_) => setState(() => _pressing = false) : null,
+            onTapCancel: _enabled ? () => setState(() => _pressing = false) : null,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              padding: widget.padding,
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: widget.borderRadius ?? BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x33000000).withAlpha((shadowOpacity * 255).round()),
+                    blurRadius: _pressing ? 2 : 4,
+                    offset: Offset(0, _pressing ? 1 : 2),
+                  ),
+                ],
+                border: _focusNode.hasFocus
+                    ? Border.all(color: theme.outline, width: 2)
+                    : null,
+              ),
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  color: fgColor.withAlpha(_enabled ? 255 : 128),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+                child: IconTheme(
+                  data: IconThemeData(
+                    color: fgColor.withAlpha(_enabled ? 255 : 128),
+                    size: 20,
+                  ),
+                  child: widget.child,
+                ),
+              ),
             ),
-          ],
-        ),
-        child: DefaultTextStyle(
-          style: TextStyle(
-            color: foregroundColor ?? theme.onPrimary,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-          ),
-          child: IconTheme(
-            data: IconThemeData(
-              color: foregroundColor ?? theme.onPrimary,
-              size: 20,
-            ),
-            child: child,
           ),
         ),
       ),
@@ -96,45 +218,109 @@ class PoddrElevatedButton extends StatelessWidget {
   }
 }
 
-class PoddrOutlinedButton extends StatelessWidget {
+class PoddrOutlinedButton extends StatefulWidget {
   final Widget child;
   final VoidCallback? onPressed;
+  final VoidCallback? onLongPress;
   final Color? borderColor;
   final Color? foregroundColor;
+  final Color? hoverBackgroundColor;
+  final BorderRadius? borderRadius;
+  final EdgeInsetsGeometry padding;
+  final String? semanticLabel;
 
   const PoddrOutlinedButton({
     super.key,
     required this.child,
     required this.onPressed,
+    this.onLongPress,
     this.borderColor,
     this.foregroundColor,
+    this.hoverBackgroundColor,
+    this.borderRadius,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+    this.semanticLabel,
   });
+
+  @override
+  State<PoddrOutlinedButton> createState() => _PoddrOutlinedButtonState();
+}
+
+class _PoddrOutlinedButtonState extends State<PoddrOutlinedButton> {
+  bool _hovering = false;
+  bool _pressing = false;
+  final _focusNode = FocusNode();
+
+  bool get _enabled => widget.onPressed != null;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = context.theme;
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: borderColor ?? theme.outline,
-          ),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: DefaultTextStyle(
-          style: TextStyle(
-            color: foregroundColor ?? theme.primary,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-          ),
-          child: IconTheme(
-            data: IconThemeData(
-              color: foregroundColor ?? theme.primary,
-              size: 20,
+    final baseBorderColor = widget.borderColor ?? theme.outline;
+    final fgColor = widget.foregroundColor ?? theme.primary;
+    final hoverBg = widget.hoverBackgroundColor ?? theme.primary.withAlpha(26);
+
+    Color bgColor = const Color(0x00000000);
+    Color borderColor = baseBorderColor;
+
+    if (_pressing) {
+      bgColor = hoverBg.withAlpha(153);
+    } else if (_hovering) {
+      bgColor = hoverBg;
+    }
+
+    if (!_enabled) {
+      borderColor = baseBorderColor.withAlpha(102);
+    }
+
+    return Semantics(
+      label: widget.semanticLabel,
+      button: true,
+      enabled: _enabled,
+      child: Focus(
+        focusNode: _focusNode,
+        child: MouseRegion(
+          cursor: _enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
+          onEnter: (_) => setState(() => _hovering = true),
+          onExit: (_) => setState(() => _hovering = false),
+          child: GestureDetector(
+            onTap: widget.onPressed,
+            onLongPress: widget.onLongPress,
+            onTapDown: _enabled ? (_) => setState(() => _pressing = true) : null,
+            onTapUp: _enabled ? (_) => setState(() => _pressing = false) : null,
+            onTapCancel: _enabled ? () => setState(() => _pressing = false) : null,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              padding: widget.padding,
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: widget.borderRadius ?? BorderRadius.circular(8),
+                border: Border.all(
+                  color: borderColor,
+                  width: _focusNode.hasFocus ? 2 : 1,
+                ),
+              ),
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  color: fgColor.withAlpha(_enabled ? 255 : 128),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+                child: IconTheme(
+                  data: IconThemeData(
+                    color: fgColor.withAlpha(_enabled ? 255 : 128),
+                    size: 20,
+                  ),
+                  child: widget.child,
+                ),
+              ),
             ),
-            child: child,
           ),
         ),
       ),
@@ -142,30 +328,89 @@ class PoddrOutlinedButton extends StatelessWidget {
   }
 }
 
-class PoddrTextButton extends StatelessWidget {
+class PoddrTextButton extends StatefulWidget {
   final String label;
   final VoidCallback? onPressed;
+  final VoidCallback? onLongPress;
   final Color? color;
+  final String? semanticLabel;
 
   const PoddrTextButton({
     super.key,
     required this.label,
     required this.onPressed,
+    this.onLongPress,
     this.color,
+    this.semanticLabel,
   });
 
   @override
+  State<PoddrTextButton> createState() => _PoddrTextButtonState();
+}
+
+class _PoddrTextButtonState extends State<PoddrTextButton> {
+  bool _hovering = false;
+  bool _pressing = false;
+  final _focusNode = FocusNode();
+
+  bool get _enabled => widget.onPressed != null;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Text(
-          label,
-          style: TextStyle(
-            color: color ?? context.theme.primary,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
+    final theme = context.theme;
+    final baseColor = widget.color ?? theme.primary;
+
+    Color textColor = baseColor;
+    if (_pressing) {
+      textColor = baseColor.withAlpha(179);
+    } else if (_hovering) {
+      textColor = baseColor.withAlpha(217);
+    }
+
+    if (!_enabled) {
+      textColor = baseColor.withAlpha(102);
+    }
+
+    return Semantics(
+      label: widget.semanticLabel ?? widget.label,
+      button: true,
+      enabled: _enabled,
+      child: Focus(
+        focusNode: _focusNode,
+        child: MouseRegion(
+          cursor: _enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
+          onEnter: (_) => setState(() => _hovering = true),
+          onExit: (_) => setState(() => _hovering = false),
+          child: GestureDetector(
+            onTap: widget.onPressed,
+            onLongPress: widget.onLongPress,
+            onTapDown: _enabled ? (_) => setState(() => _pressing = true) : null,
+            onTapUp: _enabled ? (_) => setState(() => _pressing = false) : null,
+            onTapCancel: _enabled ? () => setState(() => _pressing = false) : null,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                border: _focusNode.hasFocus
+                    ? Border.all(color: theme.outline, width: 2)
+                    : null,
+              ),
+              child: Text(
+                widget.label,
+                style: TextStyle(
+                  color: textColor,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+              ),
+            ),
           ),
         ),
       ),
--- lib/ui/components/widgets/poddr_icon_button.dart
+++ lib/ui/components/widgets/poddr_icon_button.dart
@@ -1,35 +1,118 @@
 import 'package:flutter/widgets.dart';
 import 'package:poddr/core/theme/poddr_theme.dart';
 
-class PoddrIconButton extends StatelessWidget {
+class PoddrIconButton extends StatefulWidget {
   final Widget icon;
   final VoidCallback? onPressed;
+  final VoidCallback? onLongPress;
   final double size;
+  final double? iconSize;
+  final double? padding;
   final Color? color;
+  final Color? hoverColor;
+  final Color? pressColor;
+  final BorderRadius? borderRadius;
+  final String? semanticLabel;
 
   const PoddrIconButton({
     super.key,
     required this.icon,
     required this.onPressed,
+    this.onLongPress,
     this.size = 26,
+    this.iconSize,
+    this.padding,
     this.color,
+    this.hoverColor,
+    this.pressColor,
+    this.borderRadius,
+    this.semanticLabel,
   });
 
   @override
+  State<PoddrIconButton> createState() => _PoddrIconButtonState();
+}
+
+class _PoddrIconButtonState extends State<PoddrIconButton> {
+  bool _hovering = false;
+  bool _pressing = false;
+  final _focusNode = FocusNode();
+
+  bool get _enabled => widget.onPressed != null;
+
+  double get _effectivePadding => widget.padding ?? 8;
+  double get _effectiveIconSize => widget.iconSize ?? widget.size;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      behavior: HitTestBehavior.opaque,
-      child: SizedBox(
-        width: size + 16,
-        height: size + 16,
-        child: Center(
-          child: IconTheme(
-            data: IconThemeData(
-              size: size,
-              color: color ?? context.theme.onSurface,
+    final theme = context.theme;
+    final baseColor = widget.color ?? theme.onSurface;
+    final hoverBg = widget.hoverColor ?? theme.onSurface.withAlpha(26);
+    final pressBg = widget.pressColor ?? theme.onSurface.withAlpha(51);
+
+    Color bgColor = const Color(0x00000000);
+    Color iconColor = baseColor;
+
+    if (_pressing) {
+      bgColor = pressBg;
+      iconColor = baseColor.withAlpha(204);
+    } else if (_hovering) {
+      bgColor = hoverBg;
+      iconColor = baseColor.withAlpha(230);
+    }
+
+    if (!_enabled) {
+      bgColor = const Color(0x00000000);
+      iconColor = baseColor.withAlpha(102);
+    }
+
+    final totalSize = widget.size + _effectivePadding * 2;
+    final effectiveBorderRadius = widget.borderRadius ?? BorderRadius.circular(totalSize / 2);
+
+    return Semantics(
+      label: widget.semanticLabel,
+      button: true,
+      enabled: _enabled,
+      child: Focus(
+        focusNode: _focusNode,
+        child: MouseRegion(
+          cursor: _enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
+          onEnter: (_) => setState(() => _hovering = true),
+          onExit: (_) => setState(() => _hovering = false),
+          child: GestureDetector(
+            onTap: widget.onPressed,
+            onLongPress: widget.onLongPress,
+            onTapDown: _enabled ? (_) => setState(() => _pressing = true) : null,
+            onTapUp: _enabled ? (_) => setState(() => _pressing = false) : null,
+            onTapCancel: _enabled ? () => setState(() => _pressing = false) : null,
+            behavior: HitTestBehavior.opaque,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              width: totalSize,
+              height: totalSize,
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: effectiveBorderRadius,
+                border: _focusNode.hasFocus
+                    ? Border.all(color: theme.outline, width: 2)
+                    : null,
+              ),
+              child: Center(
+                child: IconTheme(
+                  data: IconThemeData(
+                    size: _effectiveIconSize,
+                    color: iconColor,
+                  ),
+                  child: widget.icon,
+                ),
+              ),
             ),
-            child: icon,
           ),
         ),
       ),
