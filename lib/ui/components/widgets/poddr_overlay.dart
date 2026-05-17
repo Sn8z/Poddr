bc20f4ace73bf9716d9d6e0d9f3337d43405c5c8
Marcus Sneitz <marcus.sneitz@gmail.com>
2026-05-17 14:18:55 +0200

    Update poddr_overlay.dart

--- lib/ui/components/widgets/poddr_overlay.dart
+++ lib/ui/components/widgets/poddr_overlay.dart
@@ -5,11 +5,13 @@ Future<T?> showPoddrDialog<T>({
   required BuildContext context,
   required WidgetBuilder builder,
   bool dismissible = true,
+  Color? barrierColor,
 }) {
   return Navigator.of(context).push<T>(
     _PoddrDialogRoute(
       builder: builder,
       dismissible: dismissible,
+      barrierColor: barrierColor,
     ),
   );
 }
@@ -18,23 +20,23 @@ class _PoddrDialogRoute<T> extends PageRouteBuilder<T> {
   _PoddrDialogRoute({
     required WidgetBuilder builder,
     required bool dismissible,
+    Color? barrierColor,
   }) : super(
           pageBuilder: (context, animation, secondaryAnimation) =>
-              _PoddrDialogPage(builder: builder, dismissible: dismissible),
+              _PoddrDialogPage(builder: builder),
           transitionsBuilder: (context, animation, _, child) =>
               FadeTransition(opacity: animation, child: child),
           barrierDismissible: dismissible,
-          barrierColor: const Color(0x80000000),
+          barrierColor: barrierColor ?? const Color(0x80000000),
+          opaque: false,
         );
 }
 
 class _PoddrDialogPage extends StatelessWidget {
   final WidgetBuilder builder;
-  final bool dismissible;
 
   const _PoddrDialogPage({
     required this.builder,
-    required this.dismissible,
   });
 
   @override
@@ -42,17 +44,14 @@ class _PoddrDialogPage extends StatelessWidget {
     return Center(
       child: ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 400),
-        child: GestureDetector(
-          onTap: dismissible ? () => Navigator.of(context).pop() : null,
-          child: Container(
-            decoration: BoxDecoration(
-              color: context.theme.surfaceContainerLow,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            clipBehavior: Clip.antiAlias,
-            padding: const EdgeInsets.all(12),
-            child: builder(context),
+        child: Container(
+          decoration: BoxDecoration(
+            color: context.theme.surfaceContainerLow,
+            borderRadius: BorderRadius.circular(12),
           ),
+          clipBehavior: Clip.antiAlias,
+          padding: const EdgeInsets.all(12),
+          child: builder(context),
         ),
       ),
     );
